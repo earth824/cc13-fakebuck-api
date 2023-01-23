481602dfbc1b52a2cd138cde72ba8e4b40ea70f2
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2023-01-23 12:59:25 +0700

    config essentail middleware

--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const chalk = require('chalk');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 
+const notFoundMiddleware = require('./middlewares/not-found');
+const errorMiddleware = require('./middlewares/error');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(
+  rateLimit({
+    windowMs: 1000 * 60 * 15,
+    max: 1,
+    message: { message: 'too many requests, please try again later' }
+  })
+);
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 8000;
+app.listen(port, () =>
+  console.log(chalk.yellowBright.italic.bold(`server running on port: ${port}`))
+);
