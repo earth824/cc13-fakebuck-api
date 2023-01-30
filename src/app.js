8b66f4d93b7ff92d2cac3f6178e2979456c0cd14
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2023-01-30 14:00:21 +0700

    feature create post

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const rateLimit = require('express-rate-limit');
 
 const authRoute = require('./routes/auth-route');
 const userRoute = require('./routes/user-route');
+const postRoute = require('./routes/post-route');
 const friendRoute = require('./routes/friend-route');
 const authenticateMiddleware = require('./middlewares/authenticate');
 const notFoundMiddleware = require('./middlewares/not-found');
@@ -31,6 +32,7 @@ app.use(express.json());
 app.use('/auth', authRoute);
 app.use('/users', authenticateMiddleware, userRoute);
 app.use('/friends', authenticateMiddleware, friendRoute);
+app.use('/posts', authenticateMiddleware, postRoute);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
